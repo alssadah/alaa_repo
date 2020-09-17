ddbcd8089fee203ff758ba1be5034564241b69f3
alaa <ala.alssadah@gmail.com>
2020-09-17 12:55:40 +0300

    forth update

--- resources/views/annotate.blade.php
+++ resources/views/annotate.blade.php
@@ -16,6 +16,8 @@
     <body>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">OCR</a>
+        <a class="navbar-brand" href="#">Odr</a>
+        <a class="navbar-brand" href="#">Oca</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
